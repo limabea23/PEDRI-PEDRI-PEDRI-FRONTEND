cc7dd46a7503eb02b1042834eb4668e3dfc0d7e6
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-09-16 11:44:36 -0300

    refactor: API photo

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import styles from "./home.module.css";
 
@@ -20,29 +20,27 @@ export default function Page() {
         }
     };
 
+    useEffect(() => {
+        buscarListagem();
+    }, []);
+
     return (
         <div className={styles.container}>
             <h1 className={styles.titulo}>PEDRI PEDRI PEDRI</h1>
             <p className={styles.subtitulo}>Mais que um site, sua fonte número 1 sobre Pedri — o espaço definitivo para fãs conhecerem tudo sobre o Maestro do Barça e da Espanha.</p>
 
-            <div className={styles.button}>
-                <button onClick={buscarListagem} disabled={loading} className={styles.btnLiberar}>
-                    {loading ? "Carregando ..." : "Liberar a próxima história"}
-                </button>
-            </div>
-
             <div className={styles.gridListagem}>
                 {listagem.map((listagem) => (
                     <div key={listagem.idListagem} className={styles.cardListagem}> 
-                        <p>{listagem.conteudo}</p>
+                        <img 
+                            src={`http://localhost:5080/uploads/${listagem.photo}`} 
+                            alt="Foto do Pedri" 
+                            className={styles.imagemPedri}
+                        />
                     </div>
                 )
             )}
             </div>
-
-
-
-
         </div>
     );
 }
