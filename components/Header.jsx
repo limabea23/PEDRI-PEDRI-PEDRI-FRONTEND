0cee9c331d3a02ee0907c1b08d22a0639593c3b7
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-09-30 11:54:55 -0300

    refactor: header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,1 +1,27 @@
 import styles from "./header.module.css";
+import Image from "next/image";
+
+export default function Header() {
+    return (
+        <div className={styles.container}>
+            <header className={styles.header}>
+                <div className={styles.imagem}>
+                    <Image
+                    src="/pedri-header.png"
+                    alt="Logo"
+                    width={200}
+                    height={200}
+                    />
+                </div>
+                <h1 className={styles.texto}>BIOGRAFIA</h1>
+                <h1 className={styles.texto}>BARÇA</h1>
+                <h1 className={styles.texto}>SELEÇÃO ESPANHOLA</h1>
+                <h1 className={styles.texto}>GALERIA</h1>
+                <h1 className={styles.texto}>CURIOSTYLE</h1>
+                <h1 className={styles.texto}>NOTÍCIAS</h1>
+                <h1 className={styles.texto}>CADASTRO</h1>
+                <h1 className={styles.texto}>SOBRE MIM</h1>
+            </header>
+        </div>
+    );
+}
