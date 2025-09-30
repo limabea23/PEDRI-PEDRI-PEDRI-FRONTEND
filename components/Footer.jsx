b8008f84dfc09c8729e3d6ab1e48fde51d113059
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-09-30 11:54:30 -0300

    refactor: footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,8 +3,11 @@ import styles from "./footer.module.css";
 export default function Footer() {
     return (
         <div className={styles.container}>
-            <footer className={styles.footer}>
-                TODOS OS DIREITOS RESERVADOS. DESENVOLVIDO POR BEATRIZ LIMA.
+            <footer className={styles.texto}>
+                © TODOS OS DIREITOS RESERVADOS. DESENVOLVIDO POR{" "}
+                <a href="https://github.com/limabea23" target="_blank">
+                    BEA LIMA
+                </a>.
             </footer>
         </div>
     );
