a9017765f03890c03313bde059c757292763ab96
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-10-04 21:19:57 -0300

    refactor: change git hub link to sobre page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,12 @@
 import styles from "./footer.module.css";
+import Link from "next/link";
 
 export default function Footer() {
     return (
         <div className={styles.container}>
             <footer className={styles.texto}>
                 © TODOS OS DIREITOS RESERVADOS. DESENVOLVIDO POR{" "}
-                <a href="https://github.com/limabea23" target="_blank">
-                    BEA LIMA
-                </a>.
+                <Link href="/sobre">BEA LIMA</Link>
             </footer>
         </div>
     );
